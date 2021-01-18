a680666f66a7349d3cad1015a46f727821d18e01
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-18 20:10:28 +0300

    landing view

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,28 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views import View
+
+from landing.models import Landing
 
 
-class LandingView(TemplateView):
-    template_name = 'landing/index.html'
+class LandingView(View):
+    def get(self, request):
+        landing = Landing.objects.prefetch_related('row_blocks_1', 'row_blocks_2', 'column_blocks').first()
+
+        content = ''
+
+        for block in landing.row_blocks_1.all():
+            content += block.template
+
+        for block in landing.row_blocks_2.all():
+            content += block.template
+
+        for block in landing.column_blocks.all():
+            content += block.template
+
+        context = {
+            'title': landing.page_title,
+            'header': landing.header.template,
+            'content': content
+        }
+
+        return render(request, 'landing/index.html', context)
