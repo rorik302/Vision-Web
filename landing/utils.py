f4f26868ed4a65a8c35fca59c05e51e5ad4a901d
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-19 18:26:13 +0300

    sorting for blocks

--- landing/utils.py
+++ landing/utils.py
@@ -1,7 +1,13 @@
 def generate_content(blocks):
     content = ''
-
-    for block in blocks:
-        content += ''.join(b.template for b in block)
+    for block in sorted(combine_blocks(blocks), key=lambda bl: bl.sorting):
+        content += ''.join(block.template)
 
     return content
+
+
+def combine_blocks(qs_list):
+    bl = []
+    for qs in qs_list:
+        [bl.append(item) for item in qs]
+    return bl
