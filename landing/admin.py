146da0962b69f095929a8ce330d3bdb9d41e80e1
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-19 13:55:11 +0300

    admin

--- landing/admin.py
+++ landing/admin.py
@@ -1,27 +1,48 @@
 from django.contrib import admin
 
-from landing.models import Landing, Header, RowBlock_2, ColumnBlock, RowBlock_1
+from landing.models import Landing, Header, RowBlock2, ColumnBlock, RowBlock1, Navbar, NavbarMenu, NavbarMenuItem, \
+    Hero, Percent, Paragraph, NavbarContact, Benefit, Card
 
 
-class BaseStackedInline(admin.StackedInline):
+class BaseInline(admin.TabularInline):
     extra = 0
     show_change_link = True
 
 
-class RowBlock1Inline(BaseStackedInline):
-    model = Landing.row_blocks_1.through
+class MenuItemInline(BaseInline):
+    model = NavbarMenuItem
 
 
-class RowBlock2Inline(BaseStackedInline):
-    model = Landing.row_blocks_2.through
+class RowBlock1Inline(BaseInline):
+    model = RowBlock1
 
 
-class ColumnBlockInline(BaseStackedInline):
-    model = Landing.column_blocks.through
+class RowBlock2Inline(BaseInline):
+    model = RowBlock2
+
+
+class ColumnBlockInline(BaseInline):
+    model = ColumnBlock
+
+
+class ParagraphInline(BaseInline):
+    model = Paragraph
+
+
+class PercentInline(BaseInline):
+    model = Percent
+
+
+class BenefitInline(BaseInline):
+    model = Benefit
+
+
+class CardInline(BaseInline):
+    model = Card
 
 
 class LandingAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RowBlock1Inline, RowBlock2Inline, ColumnBlockInline]
 
 
 class HeaderAdmin(admin.ModelAdmin):
@@ -29,19 +50,55 @@ class HeaderAdmin(admin.ModelAdmin):
 
 
 class RowBlock1Admin(admin.ModelAdmin):
-    pass
+    inlines = [ParagraphInline, PercentInline]
 
 
 class RowBlock2Admin(admin.ModelAdmin):
+    inlines = [BenefitInline]
+
+
+class BenefitAdmin(admin.ModelAdmin):
     pass
 
 
 class ColumnBlockAdmin(admin.ModelAdmin):
+    inlines = [CardInline]
+
+
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+
+class NavbarAdmin(admin.ModelAdmin):
+    pass
+
+
+class NavbarMenuAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline]
+    exclude = ('items',)
+
+
+class NavbarMenuItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class HeroAdmin(admin.ModelAdmin):
+    pass
+
+
+class NavbarContactAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(Landing, LandingAdmin)
-admin.site.register(Header, HeaderAdmin)
-admin.site.register(RowBlock_1, RowBlock1Admin)
-admin.site.register(RowBlock_2, RowBlock2Admin)
+admin.site.register(RowBlock1, RowBlock1Admin)
+admin.site.register(RowBlock2, RowBlock2Admin)
 admin.site.register(ColumnBlock, ColumnBlockAdmin)
+admin.site.register(Header, HeaderAdmin)
+admin.site.register(Navbar, NavbarAdmin)
+admin.site.register(NavbarMenu, NavbarMenuAdmin)
+admin.site.register(NavbarMenuItem, NavbarMenuItemAdmin)
+admin.site.register(Hero, HeroAdmin)
+admin.site.register(NavbarContact, NavbarContactAdmin)
+admin.site.register(Benefit, BenefitAdmin)
+admin.site.register(Card, CardAdmin)
