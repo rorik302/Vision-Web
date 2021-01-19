a0743a0af369dd4ede7987eda072ca22cb52e742
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-19 13:55:02 +0300

    view

--- landing/views.py
+++ landing/views.py
@@ -7,36 +7,42 @@ from django.views import View
 
 from landing.forms import ContactForm
 from landing.models import Landing
+from landing.utils import generate_content
 
 
 class LandingView(View):
-    def get(self, request):
+
+    @staticmethod
+    def get(request):
+        landing = Landing.objects.select_related(
+            'header', 'header__hero', 'header__navbar',
+            'header__navbar__menu', 'header__navbar__contact') \
+            .prefetch_related(
+            'rowblock1_set',
+            'rowblock2_set',
+            'columnblock_set',
+        ).get()
+
+        content = generate_content([
+            landing.rowblock1_set.all(),
+            landing.rowblock2_set.all(),
+            landing.columnblock_set.all()
+        ])
+
         form = ContactForm()
-
-        landing = Landing.objects.prefetch_related('row_blocks_1', 'row_blocks_2', 'column_blocks').first()
-
-        content = ''
-
-        for block in landing.row_blocks_1.all():
-            content += block.template
-
-        for block in landing.row_blocks_2.all():
-            content += block.template
-
-        for block in landing.column_blocks.all():
-            content += block.template
-
-        content += render_to_string('landing/contact_form.html', {'form': form})
+        form_template = render_to_string('landing/contact_form.html', {'form': form})
 
         context = {
             'title': landing.page_title,
             'header': landing.header.template,
             'content': content,
+            'form': form_template
         }
 
         return render(request, 'landing/index.html', context)
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         form = ContactForm(request.POST)
 
         if form.is_valid():
